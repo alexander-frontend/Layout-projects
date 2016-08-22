a4e9592e2f20d9f26ecb63688d88bbef73c78fe4
sasha3333 <taekwondist4@gmail.com>
2016-08-22 17:37:46 +0300

    'commit'

--- astound/js/script.js
+++ astound/js/script.js
@@ -1,8 +1,43 @@
+$(document).ready(function(){
+	$(window).on('load resize', windowSize);
+	
+	$('.wrap_item-more_click').on('click', function(e){
+		e.preventDefault();
+		$('.overlay').addClass('opened');
+		document.body.style.overflow = "hidden";
+		$('.overlay').css('overflow','scroll')
+	});
+
+	$('.close').click(function(e){
+		e.preventDefault();
+		$('.overlay').removeClass('opened');
+		document.body.style.overflow = "auto";
+	});
+
+	$(document).on('keydown',function(e){
+		if (e.which == 27){
+			$('.overlay').removeClass('opened');
+			document.body.style.overflow = "auto";
+		}
+	});
+
+	$(".overlay").click(function(e) {
+		$(".overlay").removeClass('opened');
+		document.body.style.overflow = "auto";
+	});
+
+	$(".popup").click(function(e) {
+		e.stopImmediatePropagation();
+	});
+});
+
 function windowSize(){
-	if (window.innerWidth > '960'){
+	var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	
+	if (width > '960'){
 		$('.wrap_item').removeClass('nth-child-3np2');
 		$('.wrap_item:nth-child(3n)').addClass('nth-child-3np1');
-	} else if(window.innerWidth <= '960' && window.innerWidth > '720'){
+	} else if(width <= '960' && width > '720'){
 		$('.wrap_item').removeClass('nth-child-3np1');
 		$('.wrap_item:nth-child(2n)').addClass('nth-child-3np2');
 	} else {
@@ -10,4 +45,3 @@ function windowSize(){
 	}
 }
 
-$(window).on('load resize', windowSize);
