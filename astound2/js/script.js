ab31db22a35aba7957a2036e81474c97a44526f1
sasha3333 <taekwondist4@gmail.com>
2016-09-07 04:16:25 +0300

    'commit'

--- astound2/js/script.js
+++ astound2/js/script.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
- 
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true,
       autoPlay: true
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
   });
- 
+
+  $(".list-click").click(function(e){
+  	e.preventDefault();
+  	$(".product-description ul").slideToggle('open-list');
+  });
 });
